2dee2755428d50e9a2827c1097c950c746d5bd1f
feifeiliang <a0125994@u.nus.edu>
2015-09-17 22:55:48 +0800

    redo the CE1 code and add the function of CE2

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * TextBuddy is a program that user can  store, retrieve and delete message.
@@ -42,39 +44,45 @@ public class TextBuddy {
 
 	// error message
 	// private static final String ERROR_EMPTY = "%s is empty";
-	private static final String ERROR_BAD_ARGUMENT = "Bad argument \n %s";
+	public static final String ERROR_BAD_ARGUMENT = "Bad argument: %s \n";
 
 	// message show
-	private static final String MSG_WELCOME = "Welcome to TextBuddy. %s is ready for use";
-	private static final String MSG_ADD = "added to %s: \"%s\"";
-	private static final String MSG_CLEAR = "all content deleted from %s";
-	private static final String MSG_DELETE = "deleted from %s: \"%s\"";
-	private static final String MSG_EMPTY = "%s is empty";
-	private static final String MSG_SEARCH_NOT_FOUND = "No results fonud in %s"; 
+	public static final String MSG_WELCOME = "Welcome to TextBuddy. %s is ready for use \n";
+	public static final String MSG_ADD = "added to %s: \"%s\" \n";
+	public static final String MSG_CLEAR = "All contents are removed! \n";
+	public static final String MSG_DELETE = "deleted from %s: \"%s\" \n";
+	public static final String MSG_EMPTY = "%s is empty \n";
+	public static final String MSG_SEARCH_NOT_FOUND = "No results fonud in %s \n"; 
+	private static final String MSG_TASKS_SORTED = "All tasks have been sorted alphabetically. \n";
 
 
 	// Commands
-	private static final String COMMAND ="command: ";
-	private static final String ADD_TEXT = "add";
-	private static final String DISPLAY_TEXT = "display";
-	private static final String DELETE_TEXT = "delete";
-	private static final String CLEAR_TEXT = "clear";
-	private static final String SEARCH_TEXT = "search";
-	private static final String EXIT_PROGRAM = "exit";
+	public static final String COMMAND ="command: ";
+	public static final String ADD_TEXT = "add";
+	public static final String DISPLAY_TEXT = "display";
+	public static final String DELETE_TEXT = "delete";
+	public static final String CLEAR_TEXT = "clear";
+	public static final String SEARCH_TEXT = "search";
+	public static final String SORT_TEXT = "sort";
+	public static final String EXIT_PROGRAM = "exit";
 
 	// store all texts from the user
-	private static ArrayList <String> text; 
-	private static ArrayList <String> searchText;
+	public static ArrayList <String> text; 
 
 	// file contain texts are stored on disc
-	private static File file;
-	private static Scanner sc;
+	public static File file;
+	public static Scanner sc;
+	public static Path path;
 
 	// to store the command from user
-	private static String command = "";
-
-
-
+	public static String command = "";
+	
+	
+	public TextBuddy() {
+		path = Paths.get("textout.txt");
+		text = new ArrayList<String>();
+	}
+	
 	public static void main(String[] args) throws IOException{
 		checkArguments(args);
 		setup(args[0]);   
@@ -85,23 +93,24 @@ public class TextBuddy {
 
 	// This method is to 
 	// The user suppose to enter java program fileName
-	private static void checkArguments(String[] args){
+	public static void checkArguments(String[] args){
 		if (args.length==0){
-			messageDisplay(ERROR_BAD_ARGUMENT, "Proper Usuage is: Java program filename");
+			displayResult (String.format(ERROR_BAD_ARGUMENT, "Proper Usuage is: Java program filename"));
 			System.exit(0);
 		}
 	}
 
 	// this method is to setup the File and scanner that need to use 
-	private static void setup (String fileName){
-		file = new File(fileName);
+	public static void setup (String fileName){
+		file = new File("src/textout.txt");
 		sc = new Scanner (System.in);
 		text = new ArrayList <String> ();
+		path = Paths.get("textout.txt");
 	}
 
 	// This method is to retrieve the last save text file from user
 	// If the  
-	private static void retrieveTexts (File file){
+	public static void retrieveTexts (File file){
 		try{
 			Scanner scanner = new Scanner (file);
 			while (scanner.hasNextLine()){
@@ -112,183 +121,159 @@ public class TextBuddy {
 		}
 	}
 
-	private static void printWelcomeMessage(){
-		messageDisplay(MSG_WELCOME,file.getName());
+	public static void printWelcomeMessage(){
+		displayResult(String.format(MSG_WELCOME,path.toString()));
 	}
 
 	// This method is use to save the command that the user enter
 
-	private static void readCommandUntilExit()throws IOException{
-
-		command = readCommand();
-
-		while (!command.equals(EXIT_PROGRAM)){
-
-			switch(command){
-				case ADD_TEXT : 
-					addTexts();
-					readCommand();
-					continue;
-
-				case DELETE_TEXT : 
-					deleteTexts();
-					readCommand();
-					continue;
-
-				case DISPLAY_TEXT : 
-					displayTexts();
-					readCommand();
-					continue;
-
-				case CLEAR_TEXT : 
-					clearTexts();
-					readCommand();
-					continue;
-					
-				case SEARCH_TEXT :
-				     search();
-				     displaySearchResult();
-				     readCommand(); 
-					 break;
-
-				case EXIT_PROGRAM : 
-					break;
-
-				default: 
-					// if the user any command that cannot be identified (e.g "jump")
-					messageDisplay(ERROR_BAD_ARGUMENT, "Please enter a valid command");
-					readCommand();
-					continue;
+	public static void readCommandUntilExit()throws IOException{
+		while (true) {
+			System.out.print(COMMAND);
+			String input = sc.nextLine();
+			String output = performCommand(input);
+			if (output.equals(EXIT_PROGRAM)){
+				break;
 			}
-		}   
+			displayResult(output);
+		}
 		saveToFile(); 
 	}
+	
+	public static String performCommand(String input) throws IOException{
+		String displayMessage = null;
+	    command = getFirstWord(input);
+			switch(command){
+				case ADD_TEXT : 
+					displayMessage = addTexts(removeFirstWord(input));
+					break;
 
-	/**
-	 * This operation is used to read command from the user.
-	 * 
-	 * @return the command
-	 */
-	private static String readCommand (){
+				case DELETE_TEXT : 
+					displayMessage = deleteTexts(removeFirstWord(input));
+					break;
 
-		System.out.print(COMMAND);
-		command = sc.next().toLowerCase();
-		return command;
+				case DISPLAY_TEXT : 
+					displayMessage = displayTexts();
+					break;
 
+				case CLEAR_TEXT : 
+					displayMessage = clearTexts();
+					break;
+					
+				case SEARCH_TEXT :
+				     displayMessage = search(removeFirstWord(input));
+					 break;
+				
+				case SORT_TEXT :
+				     displayMessage = sortText();
+					 break;
+				case EXIT_PROGRAM :
+					displayMessage = EXIT_PROGRAM;
+					break;
+					 
+				default: 
+					// if the user any command that cannot be identified (e.g "jump")
+					displayMessage = String.format(ERROR_BAD_ARGUMENT, "Please enter a valid command");
+					break;
+			}
+			return displayMessage;
+	}
+
+	public static String removeFirstWord(String userCommand) {
+		return userCommand.replace(getFirstWord(userCommand), "").trim();
+	}
+
+	public static String getFirstWord(String userCommand) {
+		String commandTypeString = userCommand.trim().split("\\s+")[0].toLowerCase();
+		return commandTypeString;
 	}
 
 	// This operation is used to add the text.
-	private static void addTexts(){
-		String message = sc.nextLine().trim();
-		text.add(message);
-		messageDisplay(MSG_ADD, file.getName(), message);
-		sortText();
-
+	public static String addTexts(String message){
+		text.add(message.trim());
+		return String.format(MSG_ADD, path.toString(),message);
 	}
 
 	// This method is used to sort the arraylist alphabetically
-	private static void sortText(){
+	public static String sortText(){
 		Collections.sort(text);
+		return MSG_TASKS_SORTED + displayTexts();
 	}
 
 	// Delete command performed
-	private static void deleteTexts() throws IOException{
+	public static String deleteTexts(String message) throws IOException{
 		try{
-
-			int index = sc.nextInt();
+			int index = Integer.parseInt(message);
 			index = index-1;
 			if (index > 0 && index>=text.size()){
-				messageDisplay(ERROR_BAD_ARGUMENT, "index out of range");
-				sc.nextLine();
-				//readCommandUntilExit();
-				return;
+				return String.format(ERROR_BAD_ARGUMENT, "index out of range");
 			}
-			String message = text.get(index);
-			System.out.println(message);
 			if (text.size() == 0) {
 
-				messageDisplay(MSG_EMPTY, file.getName());
+				return String.format(MSG_EMPTY, path.toString());
 			} else {
+				message = text.get(index);
 				text.remove(index);
-				messageDisplay(MSG_DELETE, file.getName(), message);
+				return String.format(MSG_DELETE, path.toString(),message );
 			}
-		} catch (java.util.InputMismatchException e){
-			messageDisplay(ERROR_BAD_ARGUMENT, " Please enter a valid index");
-			sc.nextLine();
-			//readCommandUntilExit(); 
-			return;
+		} catch (java.lang.NumberFormatException e){
+			return String.format(ERROR_BAD_ARGUMENT, " Please enter a valid index");
 		} catch (java.lang.ArrayIndexOutOfBoundsException e){
-
-			messageDisplay(ERROR_BAD_ARGUMENT, " Please enter a valid index");
-			sc.nextLine();
-			//readCommandUntilExit(); 
-			return;
+			return String.format(ERROR_BAD_ARGUMENT, " Please enter a valid index");
 		}
 	}
 
 	// Display command performed
-	private static void displayTexts(){
-		sc.nextLine();
+	private static String displayTexts(){
 		int size = text.size();
+		String result = "";
 		if (size == 0) {
-			messageDisplay(MSG_EMPTY, file.getName());
+			result = String.format(MSG_EMPTY,path.toString());
 		} else {
-			int num=1;
+			int num = 1;
 			String label;
 			for (int i = 0; i < size; i++){
-				label = String.valueOf(num++)+".";
-				System.out.println(label + " " + text.get(i));
+				label = String.valueOf(num++) + ".";
+				result += label + " " + text.get(i) + "\n";
 			}
 		}
-
+		return result;
 	}
 
 	// Clear command performed
-	private static void clearTexts(){
-		sc.nextLine();
-		if (text.size() == 0) {
-			messageDisplay(MSG_EMPTY, file.getName()); 
-		} else {
-			text.clear();
-			messageDisplay(MSG_CLEAR, file.getName());
+		private static String clearTexts(){
+			if (text.size() == 0) {
+				return String.format(MSG_EMPTY, path.toString()); 
+			} else {
+				text.clear();
+				return String.format(MSG_CLEAR);
+			}
 		}
-	}
 
 	// Search for a word in the file
-	private static void search(){
-		String word = sc.nextLine().trim();
-		searchText = new ArrayList <String> ();
+	public static String search(String word){
+		String result = ""; 
 		int size = text.size();
 		if (size == 0){
-			messageDisplay(MSG_EMPTY, file.getName());
-		} else {
-			int i; 
-			for (i = 0; i < size; i++){
+			result = "Result not found! \n";
+		} else { 
+			for (int i = 0; i < size; i++){
 				if (text.get(i).contains(word)){
-					searchText.add(text.get(i));
+					result += (i+1) + ". "+text.get(i) + "\n";
 				}
 			}
 		}
+		return result;
 	}
 
-	// Display the search result 
-	private static void displaySearchResult (){
-	
-		int size = searchText.size();
-		if (size == 0) {
-			messageDisplay(MSG_SEARCH_NOT_FOUND, file.getName());
-		} else {
-			int num=1;
-			String label;
-			for (int i = 0; i < size; i++){
-				label = String.valueOf(num++)+".";
-				System.out.println(label + " " + searchText.get(i));
-			}
-		}
+	//This method is used to print out the message
+	//pre: message!=null
+	public static void displayResult (String message){
+		System.out.print(message);
 	}
 
 	// This method is to store all the text int he arraylist into the text file
-	private static void saveToFile() throws FileNotFoundException{
+	public static void saveToFile() throws FileNotFoundException{
 		try {
 			FileWriter fileW = new FileWriter (file);
 			BufferedWriter buffW = new BufferedWriter (fileW);
@@ -302,10 +287,10 @@ public class TextBuddy {
 
 	}
 
-
-	//This method is used to print out the error message
-	//pre: errorType!=null && message!=null
-	private static void messageDisplay(String message, Object... arguments){
-		System.out.println(String.format(message,arguments));
+	/**
+	 * @return the string form of the file passed by the user.
+	 */
+	static String showPath(){
+		return path.toString();
 	}
 }
--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -1,18 +1,79 @@
 import static org.junit.Assert.*;
-
+import java.io.IOException;
+//import org.junit.BeforeClass;
 import org.junit.Test;
+import java.util.ArrayList;
 
 public class TextBuddyTest {
 	
-	public static final String TEST_FILE = "testout.txt";
-	public static final String TEST_PATH = "src/" + TEST_FILE;
+	private static final String MSG_INVALID_TYPE = "Bad argument: Please enter a valid command \n";
+	private static final String MSG_DELETE_TASK_ERROR = "Bad argument: index out of range \n";
+	private static final String MSG_DELETED_TASK = "deleted from %1$s: \"%2$s\" \n";
+	private static final String MSG_ADDED_TASK = "added to %1$s: \"%2$s\" \n";
+	private static final String MSG_TASKS_CLEARED = "All contents are removed! \n";
 	
-	public static TextBuddy textBuddy = new TextBuddy();
-	public static
-
-	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	static ArrayList<String> expectList;
+	
+	public TextBuddyTest() {
+		expectList = new ArrayList<String>();
 	}
+	
+	public static void populateList(int numberOfItems){
+		for (int i = 0; i < numberOfItems; i++) {
+			expectList.add(String.valueOf(i+1));
+		}
+	}
+	
+	@Test
+	public void testSimpleAdd() throws IOException {
+		TextBuddy textbuddy = new TextBuddy();
+		expectList.clear();
+		populateList(1);
+		assertEquals(String.format(MSG_ADDED_TASK, textbuddy.showPath(), "1"), textbuddy.performCommand("add 1"));
+		assertEquals(expectList, TextBuddy.text);
+	}
+	
+	@Test
+	public void testSimpleDelete() throws IOException {
+		TextBuddy textbuddy = new TextBuddy();
+		expectList.clear();
+		populateList(3);
+		expectList.remove(1);
+		textbuddy.performCommand("add 1");
+		textbuddy.performCommand("add 2");
+		textbuddy.performCommand("add 3");
+		assertEquals(String.format(MSG_DELETED_TASK, textbuddy.showPath(), "2"), textbuddy.performCommand("delete 2"));
+		assertEquals(expectList, textbuddy.text);
+	}
+	
+	@Test
+	public void testDeleteTaskThatDoesNotExist() throws IOException {
+		TextBuddy textbuddy = new TextBuddy();
+		expectList.clear();
+		populateList(3);
+		textbuddy.performCommand("add 1");
+		textbuddy.performCommand("add 2");
+		textbuddy.performCommand("add 3");
+		assertEquals(MSG_DELETE_TASK_ERROR, textbuddy.performCommand("delete 4"));
+		assertEquals(expectList, textbuddy.text);
+	}
+	
+	@Test
+	public void testClear() throws IOException {
+		TextBuddy textbuddy = new TextBuddy();
+		expectList.clear();
+		textbuddy.performCommand("add 1");
+		textbuddy.performCommand("add 2");
+		textbuddy.performCommand("add 3");
+		assertEquals(MSG_TASKS_CLEARED, textbuddy.performCommand("clear"));
+		assertEquals(expectList, textbuddy.text);
+	}
+	
+	@Test
+	public void testInvalidType() throws IOException {
+		TextBuddy textbuddy = new TextBuddy ();
+		assertEquals(MSG_INVALID_TYPE, textbuddy.performCommand("asdas"));
+	}
+
 
 }
