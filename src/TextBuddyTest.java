a7d09abec0c459786e8c439bb0ad8a37db7fa469
feifeiliang <a0125994@u.nus.edu>
2015-09-17 15:00:19 +0800

    cerate test TDD

---
+++ src/TextBuddyTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextBuddyTest {
+	
+	public static final String TEST_FILE = "testout.txt";
+	public static final String TEST_PATH = "src/" + TEST_FILE;
+	
+	public static TextBuddy textBuddy = new TextBuddy();
+	public static
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
